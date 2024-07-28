bcb4f00ad1242d31f9273fc17184991b1ba7cee6
samuel <samuelhermany@hotmail.com>
2024-07-27 22:58:41 -0300

    first commit

---
+++ aula_02/src/App.jsx
@@ -0,0 +1,10 @@
+import './styles.css'
+
+export default function Padrao() {
+  return (
+    <div className="container">
+      <h1>Hello World 2</h1>
+      <h1>Hello World 2</h1>
+    </div>
+  )
+}
