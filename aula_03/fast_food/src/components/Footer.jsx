d9f8bf91b3fb54c98b9455cc3e679960d6c31cac
samuel <samuelhermany@hotmail.com>
2024-08-01 23:01:16 -0300

    Remove folder and change location folder

--- aula_03/fast_food/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Footer.module.css";
-
-export function Footer(){
-   return (
-      <footer className={styles.footer}>
-         <p>Todos os direitos reservados - 2024</p>
-      </footer>
-   )
- }
