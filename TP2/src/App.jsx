8cda5d57d4ff423ad9be3e5107d33dcc1e498edc
samuel <samuelhermany@hotmail.com>
2024-09-01 21:35:09 -0300

    create TP2 and exercise 1

--- TP2/src/App.jsx
+++ TP2/src/App.jsx
@@ -3,22 +3,22 @@ import './App.css'
 export default function App() {
     return (
         <main>
-            <a href="src/TP2/01/index.html">TP 1.1</a>
-            <a href="src/TP2/02/index.html">TP 1.2</a>
-            <a href="src/TP2/03/index.html">TP 1.3</a>
-            <a href="src/TP2/04/index.html">TP 1.4</a>
-            <a href="src/TP2/05/index.html">TP 1.5</a>
-            <a href="src/TP2/06/index.html">TP 1.6</a>
-            <a href="src/TP2/07/index.html">TP 1.7</a>
-            <a href="src/TP2/08/index.html">TP 1.8</a>
-            <a href="src/TP2/09/index.html">TP 1.9</a>
-            <a href="src/TP2/10/index.html">TP 1.10</a>
-            <a href="src/TP2/11/index.html">TP 1.11</a>
-            <a href="src/TP2/12/index.html">TP 1.12</a>
-            <a href="src/TP2/13/index.html">TP 1.13</a>
-            <a href="src/TP2/14/index.html">TP 1.14</a>
-            <a href="src/TP2/15/index.html">TP 1.15</a>
-            <a href="src/TP2/16/index.html">TP 1.16</a>
+            <a href="src/01/index.html">TP 1</a>
+            <a href="src/02/index.html">TP 2</a>
+            <a href="src/03/index.html">TP 3</a>
+            <a href="src/04/index.html">TP 4</a>
+            <a href="src/05/index.html">TP 5</a>
+            <a href="src/06/index.html">TP 6</a>
+            <a href="src/07/index.html">TP 7</a>
+            <a href="src/08/index.html">TP 8</a>
+            <a href="src/09/index.html">TP 9</a>
+            <a href="src/10/index.html">TP 10</a>
+            <a href="src/11/index.html">TP 11</a>
+            <a href="src/12/index.html">TP 12</a>
+            <a href="src/13/index.html">TP 13</a>
+            <a href="src/14/index.html">TP 14</a>
+            <a href="src/15/index.html">TP 15</a>
+            <a href="src/16/index.html">TP 16</a>
         </main>
     )
 }
