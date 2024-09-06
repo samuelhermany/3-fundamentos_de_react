80a81c34cf3a40f7cddee07ead2774997155c14a
samuel <samuelhermany@hotmail.com>
2024-09-06 19:09:17 -0300

    Projeto finalizado

--- exercicios/aula_12/src/App.jsx
+++ exercicios/aula_12/src/App.jsx
@@ -1,20 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 import { FiEdit } from "react-icons/fi";
 import { BsTrash3Fill } from "react-icons/bs";
-import styles from './App.module.css';
-import axios, { Axios } from 'axios';
-import { useEffect } from 'react';
+import axios from 'axios';
+import Modal from 'react-modal';
+
+import styles from "./app.module.css";
+
+// CSS Default
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+Modal.setAppElement("#root");
 
 export default function App(){
+  const [visibilidadeModal, setVisibilidadeModal] = useState(false);
   const [alunos, setAlunos] = useState([]);
   const [cursos, setCursos] = useState([]);
-  const [indexSelecionado, setIndexSelecionado] = useState(-1)
+  const [idSelecionado, setIdSelecionado] = useState("")
   const [formData, setFormData] = useState({
     nome: "",
     matricula: "",
     curso: "",
     bimestre: "",
   });
+
+
+  const [isLoading, setIsLoading] = useState(false);
+
 
   async function addAluno(event) {
     event.preventDefault();
@@ -33,22 +53,66 @@ export default function App(){
     }
   }
 
-  function removerAluno(posicaoArray) {
+  async function removerAluno() {
+    try {
+      await axios.delete(`https://api-aluno.vercel.app/aluno/${idSelecionado}`);
+      buscarAlunos();
+      alert("Sucesso ao remover aluno");
+      setIdSelecionado("");
+      setVisibilidadeModal(false);
+    } catch (error) {
+    alert("Erro ao remover aluno");
+    }
   }
 
   function preencherFormulario(aluno, index) {
+    console.log(aluno);
+    setFormData({
+      nome: aluno.nome,
+      matricula:aluno.matricula,
+      curso: aluno.curso,
+      bimestre: aluno.bimestre,
+    });
+
+    setIdSelecionado(aluno._id);
   }
 
-  function editarAluno(event) {
+  async function editarAluno(event) {
+    event.preventDefault();
+
+    try {
+      await axios.put(`https://api-aluno.vercel.app/aluno/${idSelecionado}`, formData
+        // ou
+        // {
+        // nome:formData.nome,
+        // matricula:formData.matricula,
+        // curso:formData.curso,
+        // bimestre:formData.bimestre,
+        // }
+      );
+      setFormData({
+        nome: "",
+        matricula: "",
+        curso: "",
+        bimestre: "",
+      })
+      setIdSelecionado("");
+      alert("Aluno editado com sucesso!");
+      buscarAlunos();
+    } catch (error) {
+      alert("Erro ao editar dados do aluno");
+    }
   }
 
   async function buscarAlunos() {
+    setIsLoading(true);
     try {
       const response = await axios.get("https://api-aluno.vercel.app/aluno");
       setAlunos(response.data);
     } catch (error) {
-      alert("Erro ao buscar dados dos alunos")
+      alert("Erro ao buscar dados dos alunos");
     }
+    setIsLoading(false);
   }
 
   async function buscarCursos() {
@@ -56,9 +120,18 @@ export default function App(){
       const response = await axios.get("https://api-aluno.vercel.app/cursos");
       setCursos(response.data.cursos);
     } catch (error) {
-      alert("Erro ao buscar cursos")
+      alert("Erro ao buscar cursos");
     }
+    // const response = axios
+    // .get("https://api-aluno.vercel.app/cursos")
+    // .then(() =>{
+    //   setCursos(response.data.cursos);
+    // })
+    // .catch(() => {
+    //   alert("Erro ao buscar cursos")
+    // });
   }
+
   // Assim que o componente for montado eu quero fazer a requisição, executa só uma vez
   useEffect(() => {
     buscarAlunos();
@@ -70,13 +143,17 @@ export default function App(){
 
   return (
     <div className={styles.container}>
-      <h1>Diário Eletrônico</h1>
+      <h1>Diário Eletrônico {idSelecionado}
+        {isLoading ? <p>Carregando...</p>:null}
+      </h1>
 
       <form
         className={styles.form}
         // Se eu tenho um index selecionado chamo editarAluno, senão addAluno
         onSubmit={(event) =>
-          indexSelecionado >= 0 ? editarAluno(event) : addAluno(event)}>
+          idSelecionado ? editarAluno(event) : addAluno(event)}>
+            {/* ou
+            idSelecionado != "" ? editarAluno(event) : addAluno(event)}> */}
         <input
           placeholder="Nome"
           value={formData.nome}
@@ -117,38 +194,65 @@ export default function App(){
       <div className={styles.container_table}>
         <h3>Alunos Cadastrados</h3>
         {alunos.length > 0 ? <table className={styles.table}>
-          <tr>
-            <th>Ordem</th>
-            <th>Nome</th>
-            <th>Matrícula</th>
-            <th>Curso</th>
-            <th>Bimestre</th>
-            <th>Ações</th>
-          </tr>
-
-          {alunos.map((item, index) => (
+          <thead>
             <tr>
-              <td className={styles.center}>{index + 1}</td>
-              <td className={styles.center}>{item.nome}</td>
-              <td className={styles.center}>{item.matricula}</td>
-              <td className={styles.center}>{item.curso}</td>
-              <td className={styles.center}>{item.bimestre}</td>
-              <td className={styles.center}>
-                <FiEdit
-                  color='#0FBA3F'
-                  cursor="pointer"
-                  onClick={() => preencherFormulario(item, index)}
-                />
-                <BsTrash3Fill
-                  color='#F90000'
-                  cursor="pointer"
-                  onClick={() => removerAluno(index)}
-                />
-              </td>
+              <th>Ordem</th>
+              <th>Nome</th>
+              <th>Matrícula</th>
+              <th>Curso</th>
+              <th>Bimestre</th>
+              <th>Ações</th>
             </tr>
-          ))}
+          </thead>
+
+          <tbody>
+            {alunos.map((aluno, index) => (
+              <tr key={aluno._id}>
+                <td className={styles.center}>{index + 1}</td>
+                <td className={styles.center}>{aluno.nome}</td>
+                <td className={styles.center}>{aluno.matricula}</td>
+                <td className={styles.center}>{aluno.curso}</td>
+                <td className={styles.center}>{aluno.bimestre}</td>
+                <td className={styles.center}>
+                  <FiEdit
+                    color='#0FBA3F'
+                    cursor="pointer"
+                    onClick={() => preencherFormulario(aluno)}
+                  />
+                  <BsTrash3Fill
+                    color='#F90000'
+                    cursor="pointer"
+                    onClick={() => {
+                      setIdSelecionado(aluno._id);
+                      setVisibilidadeModal(true);
+                    }}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>:<h4>Nenhum aluno cadastrado</h4>}
       </div>
+      <Modal
+        isOpen={visibilidadeModal}
+        style={customStyles}
+        onRequestClose={() => {
+          setVisibilidadeModal(false);
+          setIdSelecionado("");
+        }}
+        >
+        <h2 style={{color: "#000"}}>Confirmação</h2>
+        <p style={{color: "#000"}}>Deseja remover o item selecionado</p>
+        <button onClick={() =>removerAluno()}>Sim</button>
+        <button
+          onClick={() => {
+          setVisibilidadeModal(false);
+          setIdSelecionado("");
+          }}
+        >
+          Não
+        </button>
+      </Modal>
     </div>
   )
 }
